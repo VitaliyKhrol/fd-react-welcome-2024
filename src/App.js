bc25493571ff29cb53232e44b9f6a80d681029af
Vitaliy K <vitaliyx@gmail.com>
2024-05-29 10:05:13 +0300

    Улучшение структуры компонентов

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import React from "react";
-import {userData} from "./components/Greeting/userData";
-import UserCard from "./components/UserCard";
-import './components/UserCard/style.css'
-
+import UserDashboard from "./components/UserDashboard";
 
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      users: userData,
-      isSort: true,
-    }
-  }
 
-userMap =()=> this.state.users.map((userObj)=> < UserCard user ={userObj} userkey= {userObj.id}/>)
-
-
-sortUsers =()=>{
-  console.log(this.state)
-    const newUsers =[...this.state.users];
-    newUsers.sort((a,b)=>(a.name>b.name && this.state.isSort ? 1 : -1));
-
-    this.setState({
-      users: newUsers,
-      isSort:!this.state.isSort
-    })
-    }
 
   render() {
     return (
-      <section className="root">
-        <button onClick={this.sortUsers}>Sorted</button>
-        <div className="card-container">
-           {this.userMap()}
-        </div>
-       
-          </section>
+      <UserDashboard />
     )
   }
 }
---
+++ src/components/UserDashboard/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {userData} from "./userData";
+import UserCard from '../UserCard';
+
+
+
+class UserDashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: userData,
+            isSort: true,
+        }
+    }
+
+
+    userMap = () => this.state.users.map((userObj) => < UserCard user={userObj} userkey={userObj.id} />)
+
+    sortUsers = () => {
+        console.log(this.state)
+        const newUsers = [...this.state.users];
+        newUsers.sort((a, b) => (a.name > b.name && this.state.isSort ? 1 : -1));
+
+        this.setState({
+            users: newUsers,
+            isSort: !this.state.isSort
+        })
+    }
+
+
+    render() {
+        return (
+            <section className="root">
+                <button onClick={this.sortUsers}>Sorted</button>
+                <div className="card-container">
+                    {this.userMap()}
+                </div>
+
+            </section>
+        );
+    }
+}
+
+export default UserDashboard;
