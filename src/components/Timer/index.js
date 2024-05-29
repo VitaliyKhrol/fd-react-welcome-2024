487ac07149535e8978a56f39a85e1677618ce785
Vitaliy K <vitaliyx@gmail.com>
2024-05-29 16:06:07 +0300

    Секундомер с одной кнопкой

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,48 +1,63 @@
 import React, { Component } from 'react';
-import { format,addSeconds } from 'date-fns';
+import { format, addSeconds } from 'date-fns';
 
 
 class Timer extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            time: new Date(0,0,0,0,0,0,0)
+        this.state = {
+            time: new Date(0, 0, 0, 0, 0, 0, 0),
+            isRunning: true,
         }
-        this.intervalId=null;
+        this.intervalId = null;
+
     }
 
-    start =()=>{
-        this.intervalId = setInterval(()=>{
-         const {time} =this.state;
-         const newdate =addSeconds(time,1)
-             this.setState({
-                time: newdate
-             })
-
-        },1000 );
-     
-    }
-
-    componentDidMount(){
-        // this.start();
-    }
-
-    stop=()=>{
-         clearInterval(this.intervalId)
-       }
-
-     componentWillUnmount(){
-        clearInterval(this.intervalId)
-     }  
+    start = () => {
+            this.intervalId = setInterval(() => {
+            const { time } = this.state;
+            const newdate = addSeconds(time, 1)
+            this.setState({
+                time: newdate,
+                isRunning:false,
+            })
+        }, 1000);
+      }
 
     
+    stop = () => {
+        clearInterval(this.intervalId)
+        this.setState({
+            isRunning:true,
+        })
+    }
+
+    clickHandler=()=>{
+        const {isRunning } = this.state;
+        if (isRunning){
+            this.start()
+        } else {this.stop()}
+
+    }
+
+    clear = ()=>{
+        this.stop();
+        this.setState({
+            time: new Date(0, 0, 0, 0, 0, 0, 0),
+        })
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId)
+    }
+
     render() {
-        const {time} =this.state;
+        const { time,isRunning} = this.state;
         return (
             <div>
                 <h1>{format(time, 'HH:mm:ss')}</h1>
-                <button onClick={this.start}>Start</button>
-                <button onClick={this.stop}>Stop</button>
+                <button onClick={this.clickHandler}>{isRunning ? 'Start' : 'Stop'}</button>
+                <button onClick={this.clear}>CLEAR</button>
             </div>
         );
     }
