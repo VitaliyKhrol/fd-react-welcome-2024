8754cc96a7ab72ef51966f085bbda9e3616ace9e
Vitaliy K <vitaliyx@gmail.com>
2024-06-15 17:12:38 +0300

    React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
+import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
+import LoginForm from './components/LoginForm';
+import TodoList from './components/Todolist';
 
-
-import UserCard from "./components/UserCard";
-import WindowResazer from "./components/WindowResazer";
 
 
 class App extends React.Component {
@@ -15,12 +15,32 @@ class App extends React.Component {
   render() {
 
     return (
-      <>
-      <WindowResazer/>
-      </>
+      <BrowserRouter>
+      <ul>
+          <li> <Link to="/">Go to Home</Link>  </li> 
+          <li> <Link to="/login">Go to LoginForm</Link> </li>
+          <li> <Link to="/todo">Go to ToDo</Link></li>
+      </ul> 
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<LoginForm/>}/>
+          <Route path="/todo" element={<TodoList/>}/>
+          <Route path="*" element={<NotFound/>}/>
+
+        </Routes>
+
+      </BrowserRouter>
     )
   }
 }
 
 
 export default App;
+
+const NotFound =()=>{
+  return <div>Page not Found</div>
+}
+
+const Home =()=>{
+  return <h1>You are home now</h1>
+}
