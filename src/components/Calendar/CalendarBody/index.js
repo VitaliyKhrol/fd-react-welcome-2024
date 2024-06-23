5491158cc6bb57ee1899bf8448d6fcb6ade621dc
Vitaliy K <vitaliyx@gmail.com>
2024-06-23 12:41:12 +0300

    Calendar task измениния

--- src/components/Calendar/CalendarBody/index.js
+++ src/components/Calendar/CalendarBody/index.js
@@ -2,22 +2,29 @@ import React from 'react';
 import Mount from './Mouth';
 
 
-const CalendarBody = () => {
+const CalendarBody = (props) => {
 
 
-    const weekDays =['S','M','T','W','T','F','S'];
+    const weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
     return (
-        <table>
-            <thead>
-                <tr style = {{fontWeight:'bold'}}>
-                    {weekDays.map(d => <td>{d}</td>)}
-                </tr>
-            </thead>
-            
-                <Mount mount={6}/>
-           
-            
-        </table>
+
+        <div style={{display:'flex',width:'100%',margin:'20px', flexDirection:'column' ,alignItems:'center'}}>
+            <p>2023</p>
+            <table>
+                <thead>
+                    <tr style={{ fontWeight: 'bold' }}>
+                        {weekDays.map(d => <td>{d}</td>)}
+                    </tr>
+                </thead>
+
+                <Mount mount={6} />
+
+
+            </table>
+
+
+        </div>
+
     );
 }
 
