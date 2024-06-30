16a0336a45ccbdcbc8b4d1f4320bf6d70ca0f3ad
Vitaliy K <vitaliyx@gmail.com>
2024-06-30 13:50:40 +0300

    source array

--- src/components/MouseTracker/index.js
+++ src/components/MouseTracker/index.js
@@ -2,26 +2,29 @@
 import React, { useEffect, useState } from 'react';
 
 const Counter = () => {
-
     const [count, setCount] = useState(0);
-
-
+    const [step, setStep] = useState(0);
 
     useEffect(() => {
         const handleClick = () => {
-        setCount((count)=> count+1)}
+            setCount((count)=> count + step)
+        }
+        console.log('effect created')
         window.addEventListener('click', handleClick)
-        return ()=>{
+        return () => {
             window.removeEventListener('click', handleClick)
         }
-    },[]);
+    }, [step]);
 
-
+    const handleStep = ({ target: { name, value } }) => {
+        setStep(Number(value));
+    }
 
     return (
         <div>
             <p>Counter: {count}</p>
-           </div>
+            <input type='text' name='step' value={step} onChange={handleStep} />
+        </div>
     );
 }
 
